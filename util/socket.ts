4479d53314bdbbaa833abe3d64bf041428a90da0
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-07-19 17:22:27 +0700

    update transport socket

--- util/socket.ts
+++ util/socket.ts
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
 const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
-const socket = io(`${server_url}`, { autoConnect: false });
+const socket = io(`${server_url}`, {
+  autoConnect: false,
+  transports: ["websocket", "polling"],
+});
 
 socket.onAny((event, ...args) => {
   console.log(event, args, "onAny");
