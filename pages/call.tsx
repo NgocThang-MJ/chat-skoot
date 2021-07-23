85acbadccc3392eb923b969e88fcb760c24a915e
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-07-23 10:39:37 +0700

    implement toggle video & audio

--- pages/call.tsx
+++ pages/call.tsx
@@ -1,9 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import Peer from "simple-peer";
+import Peer, { Instance } from "simple-peer";
 import { GrClose } from "react-icons/gr";
-import { FaPhone } from "react-icons/fa";
+import {
+  FaPhone,
+  FaMicrophone,
+  FaMicrophoneAltSlash,
+  FaVideoSlash,
+  FaVideo,
+} from "react-icons/fa";
 
 import socket from "../util/socket";
 
@@ -14,9 +20,15 @@ export default function Call() {
   const [talkerImg, setTalkerImg] = useState("");
   const [calling, setCalling] = useState(false);
   const [inCall, setInCall] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isOffVideo, setIsOffVideo] = useState(false);
+  const peerRef = useRef<Instance>();
+  const streamRef = useRef<MediaStream>();
+  const audioTrackRef = useRef<MediaStreamTrack>();
+  const videoTrackRef = useRef<MediaStreamTrack>();
   const friendsVideoRef = useRef<HTMLVideoElement>(null);
   const myVideoRef = useRef<HTMLVideoElement>(null);
-  const audioRef = useRef<HTMLAudioElement>();
+  const ringtoneRef = useRef<HTMLAudioElement>();
 
   const offCall = () => {
     socket.emit("off call", room_id);
@@ -28,6 +40,24 @@ export default function Call() {
     socket.emit("end call", room_id);
   };
 
+  const toggleAudio = () => {
+    if (audioTrackRef.current) {
+      isMuted
+        ? (audioTrackRef.current.enabled = true)
+        : (audioTrackRef.current.enabled = false);
+      setIsMuted(!isMuted);
+    }
+  };
+
+  const toggleVideo = () => {
+    if (videoTrackRef.current) {
+      isOffVideo
+        ? (videoTrackRef.current.enabled = true)
+        : (videoTrackRef.current.enabled = false);
+      setIsOffVideo(!isOffVideo);
+    }
+  };
+
   const clearStorage = () => {
     localStorage.removeItem("data");
     localStorage.removeItem("name_caller");
@@ -36,6 +66,8 @@ export default function Call() {
     localStorage.removeItem("img_talker");
   };
 
+  // Effect
+
   useEffect(() => {
     const name_talker = localStorage.getItem("name_talker");
     const image_talker = localStorage.getItem("img_talker");
@@ -43,13 +75,14 @@ export default function Call() {
       clearStorage();
       return window.close();
     }
-    audioRef.current = new Audio("/RenaiCirculation.mp3");
+    ringtoneRef.current = new Audio("/RenaiCirculation.mp3");
     setTalkerName(name_talker);
     setTalkerImg(image_talker);
   }, []);
 
   useEffect(() => {
     if (!router.isReady) return;
+    console.log(isOffVideo);
     socket.connect();
     socket.on("connect", () => {
       socket.emit("join conversation", room_id);
@@ -62,7 +95,18 @@ export default function Call() {
         audio: audio === "true",
       })
       .then((stream) => {
+        streamRef.current = stream;
+        console.log(stream);
         const tracks = stream.getTracks();
+        const audioTrack = stream.getAudioTracks()[0];
+        const videoTrack = stream.getVideoTracks()[0];
+        audioTrackRef.current = audioTrack;
+        videoTrackRef.current = videoTrack;
+        const audioTracks = stream.getAudioTracks();
+        console.log(audioTracks);
+        console.log(videoTrack);
+        console.log(audioTrack);
+        // console.log(tracks);
 
         socket.on("end call", () => {
           setInCall(false);
@@ -77,6 +121,8 @@ export default function Call() {
             trickle: false,
             stream: stream,
           });
+
+          peerRef.current = peer;
 
           const remote_data = localStorage.getItem("data");
           if (!remote_data) {
@@ -99,14 +145,16 @@ export default function Call() {
 
           peer.signal(remote_data);
         } else {
-          audioRef.current ? (audioRef.current!.currentTime = 0) : null;
-          audioRef.current && audioRef.current.play();
+          ringtoneRef.current ? (ringtoneRef.current!.currentTime = 0) : null;
+          ringtoneRef.current && ringtoneRef.current.play();
           setCalling(true);
           const peer = new Peer({
             initiator: true,
             trickle: false,
             stream: stream,
           });
+
+          peerRef.current = peer;
 
           peer.on("signal", (data) => {
             socket.emit("call", {
@@ -120,7 +168,7 @@ export default function Call() {
           });
 
           peer.on("stream", (remoteStream) => {
-            audioRef.current && audioRef.current.pause();
+            ringtoneRef.current && ringtoneRef.current.pause();
             myVideoRef.current!.srcObject = stream;
             friendsVideoRef.current!.srcObject = remoteStream;
           });
@@ -185,11 +233,46 @@ export default function Call() {
           <div className="mx-auto h-full flex items-center justify-center">
             <div className="flex flex-col items-center w-full h-full">
               <div className="flex items-center justify-center w-full h-5/6">
-                <div className="mx-2 w-2/5 h-5/6">
-                  <video autoPlay muted playsInline ref={myVideoRef} />
+                <div className="mx-2 w-2/5 h-5/6 relative">
+                  <video
+                    className="max-w-full max-h-full"
+                    autoPlay
+                    muted
+                    playsInline
+                    ref={myVideoRef}
+                  />
+                  <div className="flex justify-between mx-2 absolute bottom-0 mb-2 w-full">
+                    <div
+                      onClick={toggleAudio}
+                      className="p-3 bg-gray-500 rounded-full opacity-60 cursor-pointer"
+                    >
+                      {isMuted ? (
+                        <FaMicrophoneAltSlash className="text-gray-300 h-6 w-6" />
+                      ) : (
+                        <FaMicrophone className="text-gray-300 h-6 w-6" />
+                      )}
+                    </div>
+                    {video === "true" && (
+                      <div
+                        onClick={toggleVideo}
+                        className="p-3 bg-gray-500 rounded-full opacity-60 cursor-pointer"
+                      >
+                        {isOffVideo ? (
+                          <FaVideoSlash className="text-gray-300 h-6 w-6" />
+                        ) : (
+                          <FaVideo className="text-gray-300 h-6 w-6" />
+                        )}
+                      </div>
+                    )}
+                  </div>
                 </div>
                 <div className="mx-2 w-2/5 h-5/6">
-                  <video autoPlay playsInline ref={friendsVideoRef} />
+                  <video
+                    className="max-w-full max-h-full"
+                    autoPlay
+                    playsInline
+                    ref={friendsVideoRef}
+                  />
                 </div>
               </div>
 
