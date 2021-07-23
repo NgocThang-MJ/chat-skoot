dd62de3f4f486d3753892076a44f54e2073294ee
NgocThang-MJ <tranngocthang.bkdn@gmail.com>
2021-07-23 17:41:30 +0700

    Try to fix scroll in mobile

--- components/message/Chat.tsx
+++ components/message/Chat.tsx
@@ -240,7 +240,7 @@ export default function Chat(props: {
           </div>
           <div
             ref={chatBoxRef}
-            className="flex flex-col-reverse flex-grow h-72 max-h-full overflow-y-auto px-4 py-2 transition-all"
+            className="flex flex-col-reverse flex-grow h-72 max-h-full overflow-y-scroll lg:overflow-y-auto px-4 py-2 transition-all"
           >
             {/* {room &&
               room?.members.map((member) => (
